3df111569f009c41ec794d653485a7f7a9055b2f
Kapil Israni <090max@gmail.com>
2019-11-23 22:56:52 +0530

    Added the Final python flask code

--- app.py
+++ app.py
@@ -8,108 +8,251 @@ import turicreate as tc
 sys.path.append("..")
 import json
 from flask_cors import CORS
+from flask import request
+from datetime import datetime
+import json as json
+from pymongo import MongoClient
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.ensemble import RandomForestRegressor
+from bson import ObjectId
+import math
 
 app=Flask(__name__)
 CORS(app)
 
-def create_data_dummy(data):
-    data_dummy = data.copy()
-    data_dummy['purchase_dummy'] = 1
-    return data_dummy
+url='mongodb+srv://test:test@cluster0-nihvn.mongodb.net/test?retryWrites=true&w=majority'
+db_name='shop_list'
 
-def split_data(data):
-    '''
-    Splits dataset into training and test set.
-    
-    Args:
-        data (pandas.DataFrame)
+
+def read_json(url,db_name,table_name):
+    client = MongoClient(url)
+    db = client.get_database(db_name)
+    if(table_name=="customers"):
+        return(db.customers)
+    elif(table_name=="transaction"):
+        return(db.transaction)
+    elif(table_name=="itemlist"):
+        return(db.itemlist)
+    elif(table_name=="category"):
+        return(db.category)
+    elif(table_name=="rta"):
+        return(db.rta)
+
+#functions for recommendation -->>
+#To get the overall users list
+def get_user():
+    users_table=read_json(url,db_name,"customers")
+    res=users_table.find({},{"_id":0})
+    users=[]
+    for i in res:
+        users.append(str(i["cust_id"]))
+    return users
+
+#To get the the data for recommendation
+def get_data(users):
+    user_data=[]#output 1
+    item_data=[]#output 2
+    target_data=[]#output 3
+
+    transactions_table=read_json(url,db_name,"transaction")
+
+    for user in users:
+        #An object to find in the table
+        query={}
+        query["cust_id"]=int(user)
         
-    Returns
-        train_data (tc.SFrame)
-        test_data (tc.SFrame)
-    '''
-    train, test = train_test_split(data, test_size = .2)
-    train_data = tc.SFrame(train)
-    test_data = tc.SFrame(test)
-    return train_data, test_data
+        res=transactions_table.find(query,{"_id":0,"cust_id":0})#ignoring the _id and cust_id fields
+        for obj in res:
+            for enteries in obj["Transaction"]:
+                user_data.append(str(user))
+                item_data.append(str(enteries["item_id"]))
+                target_data.append(len(enteries["item_transactions"]))
+    return user_data,item_data,target_data
+    
+#Functions for prediction algorithms -->>
+def calc_error(predicted,actual):
+    error=0
+    for i in range(0,len(actual)):
+        error=error+((actual[i]-predicted[i])*(actual[i]-predicted[i]))
+    error=error/len(actual)
+    return math.sqrt(error)
 
-def model(train_data,name,user_id,item_id,target,users_to_recommend,n_rec,n_display):
-    if name=='popularity':
-        model=tc.popularity_recommender.create(train_data,user_id=user_id,item_id=item_id,target=target)
-    elif name=='cosine':
-        model = tc.item_similarity_recommender.create(train_data, user_id=user_id, item_id=item_id, target=target,similarity_type='cosine')
-    elif name == 'pearson':
-        model = tc.item_similarity_recommender.create(train_data,user_id=user_id,  item_id=item_id,  target=target,similarity_type='pearson')
-    recom = model.recommend(users=users_to_recommend, k=n_rec)
-    recom.print_rows(n_display)
-    return model
+def removeOutliers(frequency,threshold):
+    modified_freq=[]
+    modified_quantity=[]
+
+    for freq,arr in frequency.items():
+        if(len(arr)==1):
+            modified_freq.append(freq)
+            modified_quantity.append(arr[0])
+        else:
+            z=stats.zscore(arr)
+            for idx in range(0,len(z)):
+                if(np.isnan(z[idx])==True):
+                    modified_freq.append(freq)
+                    modified_quantity.append(arr[idx])
+                elif(abs(z[idx])<threshold):
+                    modified_freq.append(freq)
+                    modified_quantity.append(arr[idx])
+    return modified_freq,modified_quantity
+
+    
+def get_dates_quantity(dates,quantity,remove_outliers=0,outliers_threshold=0):
+    dates_arr=[]
+    frequency_distribution={} 
+    for i in range(1,len(dates)):
+        frequency=(dates[i]-dates[i-1]).astype('int64')
+        dates_arr.append(frequency)
+        frequency_distribution[frequency]=[]
+
+    quantity=quantity[1:]
+  
+    if(remove_outliers==1):
+        for idx in range(0,len(dates_arr)):
+            frequency_distribution[dates_arr[idx]].append(quantity[idx])
+            modified_dates,modified_quantity=removeOutliers(frequency_distribution,outliers_threshold)
+        modified_dates=np.array(modified_dates).astype('int64')
+        modified_dates=np.reshape(modified_dates,(len(modified_dates),1))
+        return modified_dates,modified_quantity
+    else:
+        dates_arr=np.array(dates_arr).astype('int64')
+        dates_arr=np.reshape(dates_arr,(len(dates_arr),1))
+    return (dates_arr,quantity)
+
+def algo(dates,quantity,gap):
+    dates = np.array(dates).astype('datetime64[D]')
+    #preparing frequncy array(dates_arr)
+    (dates_arr , quantity) = get_dates_quantity(dates,quantity,1,1.5)
+
+    #INITIALISING THE MODEL
+    
+    svr_rbf=SVR(kernel='rbf',C=1e3,gamma=0.1)
+    random_forest = RandomForestRegressor(n_estimators=5,random_state=10)
+
+     #FITTING THE MODEL
+    #svr_poly.fit(dates_arr,quantity)-- CURRENTLY NOT USING POLY
+    svr_rbf.fit(dates_arr,quantity)
+    random_forest.fit(dates_arr,quantity);
+
+    #READING THE CURRENT TIMESTAMP TO FIND THE GAP
+    predict_dates = gap
+    predict_dates = np.reshape(predict_dates,(1,1))
+ 
+    #PREDICTING FROM THE FITTED MODEL
+    if predict_dates > max(dates_arr):
+        maximum = max(dates_arr)[0]
+        k = 0
+        max_quant = 0
+        for i in dates_arr:
+            if (i[0] == maximum):
+                if (quantity[k] > max_quant):
+                    max_quant = quantity[k]
+            k += 1
+    return(round(max_quant))
+
+    rbf= svr_rbf.predict(dates_arr)
+    rf=random_forest.predict(dates_arr)#rf=Random Forest
+  
+    rounded_rbf=[]
+    rounded_rf=[]
+
+    for i in range(0,len(rbf)):
+        rounded_rbf.append(round(rbf[i]))
+        rounded_rf.append(round(rf[i]))
+    error_rbf=calc_error(rounded_rbf,quantity)
+    error_rf=calc_error(rounded_rf,quantity)
+    #print(error_rbf,error_rf) -->> ERROR PRINTING
+    if(error_rbf<=error_rf):
+        return svr_rbf.predict(predict_dates)[0]
+    else:
+        return random_forest.predict(predict_dates)[0]
+
+def JsonPrediction(transaction,itemID,gap,user):
+    search_user={}
+    search_user["cust_id"]=user
+    item = transaction.find(search_user,{"Transaction.item_transactions.date":1, "Transaction.item_transactions.quantity":1,"Transaction.item_id":1,"_id":0}).sort("Transaction.item_transactions.date")
+    for x in item:
+        for y in x["Transaction"]:
+            dates = []
+            quantity = []
+      
+        if(y['item_id']==itemID):
+            item_trans = y['item_transactions']
+            
+            for z in item_trans:
+                dates.append(z['date'])
+                quantity.append(z['quantity'])
+            ans = algo(dates,quantity,gap)       
+        return ans
 
 @app.route('/ml/recommend',methods=['GET'])
-def recommned():
-    customers = pd.read_csv('./data/recommend_1.csv') 
-    transactions = pd.read_csv('./data/trx_data.csv')
+#Main function for recommendation
+def recommend():
+    userid = request.args.get('userid')
+    users=get_user()
+    #users=[25]
+    user_data,item_data,target_data=get_data(users)
 
-    transactions['products'] = transactions['products'].apply(lambda x: [int(i) for i in x.split("|")])
-    transactions.head(2).set_index('customerId')['products'].apply(pd.Series).reset_index()
+    user_arr=[]
+    user_arr.append(str(user_id))
 
-    data = pd.melt(transactions.set_index('customerId')['products'].apply(pd.Series).reset_index(), 
-               id_vars=['customerId'],
-                 value_name='products') \
-        .dropna().drop(['variable'], axis=1) \
-        .groupby(['customerId', 'products']) \
-        .agg({'products': 'count'}) \
-        .rename(columns={'products': 'purchase_count'}) \
-        .reset_index() \
-        .rename(columns={'products': 'productId'})
+    sf = tc.SFrame({'user_id':user_data,'item_id':item_data,'frequency':target_data})
+    m = tc.item_similarity_recommender.create(sf,target="frequency",similarity_type='cosine')
+    #recom=m.recommend(users,k=10) UNCOMMENT IF want to test for all users
+    recom=m.recommend(user_arr,k=10)
+    output={}
+    output["item_id"]=[]
 
+    for items in recom["item_id"]:
+      output["item_id"].append(items)
 
-    data['productId']=data['productId'].astype(np.int64)
-
-
-    data_dummy = create_data_dummy(data)
-
-    df_matrix = pd.pivot_table(data, values='purchase_count', index='customerId', columns='productId')
-
-#df_matrix_norm = (df_matrix-df_matrix.min())/(df_matrix.max()-df_matrix.min())
-    df_matrix_norm=df_matrix
-
-# create a table for input to the modeling  
-    d = df_matrix_norm.reset_index() 
-    d.index.names = ['scaled_purchase_freq'] 
-    data_norm = pd.melt(d, id_vars=['customerId'], value_name='scaled_purchase_freq').dropna()
-    
-    train_data, test_data = split_data(data)
-    train_data_dummy, test_data_dummy = split_data(data_dummy)
-    train_data_norm, test_data_norm = split_data(data_norm)
-
-# constant variables to define field names include:
-    user_id = 'customerId'
-    item_id = 'productId'
-    users_to_recommend = list(customers[user_id])
-    n_rec = 10 # number of items to recommend
-    n_display = 30 # to display the first few rows in an output dataset
+    return json.dumps(output)
 
 
-    final_model = tc.item_similarity_recommender.create(tc.SFrame(data_norm),user_id=user_id, item_id=item_id,target='scaled_purchase_freq', similarity_type='cosine')
+@app.route('/ml/predict',methods=['GET'])
+def predict():
+    userid = request.args.get('userid')
+    transaction =read_json(url,db_name,"transaction")
+    rta = read_json(url,db_name,"rta")#Getting the rta table
 
-    recom = final_model.recommend(users=users_to_recommend, k=n_rec)
-    recom.print_rows(n_display)
+    # itemlist = db.itemlist
+    user_dict={}
+    user_dict["cust_id"]=userid
+    itemDetails = rta.find(user_dict,{'item_details.avg':1 ,'item_details.last_date':1,'_id':0 , 'item_details.item_id':1 })#Mongo query
 
+    output = []
+    for item in itemDetails:
+        for one_item in item['item_details']:
+            avg = one_item['avg'] #Fetch the avg of an item for a particular user
+            datetimeobj = datetime.datetime.now()
 
-    json_output=[]
-    temp_inc=0
-    for i in recom:
-        if(i['score']!=0):
-            i['key']=temp_inc
-            temp_inc=temp_inc+1
-            json_output.append(i)
+            date = datetimeobj.strftime("%Y") + "-" +datetimeobj.strftime("%m") + "-" + datetimeobj.strftime("%d")
+        
+            last_date_of_purchase=one_item['last_date']
+        
+        
+            t = (datetime.datetime.strptime(date,"%Y-%m-%d") - datetime.datetime.strptime(last_date_of_purchase,"%Y-%m-%d"))
+        
+            t = t.days
+            avg=round(avg)
+            if(avg !=0 and ((avg)-2)<=t and t<=(avg+3)):
+                item_pred = {}
+                itemid = one_item['item_id']
+                ans = JsonPrediction(transaction=transaction,itemID=itemid,gap=t)
+            
+                dictionary = dict({'item_id' : itemid})
 
-
-    final_output=json.dumps(json_output)
-    return final_output
-
-
-
+            # itemName = itemlist.find( dictionary, {'item_name':1 ,'item_id':1, '_id':0})
+                item_pred['itemID'] = itemid
+            # for name in itemName['item_name']:
+                item_pred['itemName'] = "Test_items"
+                item_pred['Quantity'] = round(ans)
+                output.append(item_pred)
+            
+    json_output=json.dumps(output)
+    return json_output
 
 
 
